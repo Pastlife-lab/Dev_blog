e2febed5978499ba7828315cd6b3c2e2aabeb010
Pastlife-lab <omikron36@hotmail.com>
2026-05-19 10:27:04 +0200

    a

--- dev-blog/app/pages/contacts.vue
+++ dev-blog/app/pages/contacts.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Kontakt</h1>
-            <p>Du kan nå meg på <a href="mail.mail.no">Mail</a></p>
+            <p>Du kan nå meg på <a href="mailto:mail@mail.no">Mail (link to: Mail@mail.no)</a></p>
     <AppNav />
     </div>
 </template>
